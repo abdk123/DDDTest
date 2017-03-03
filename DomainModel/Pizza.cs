9a8854438c1a0ea41889f01a16975f24021d0856
abdok1234 <abdok1234@gmail.com>
2017-03-03 18:09:51 +0200

    3-3-2017

--- DomainModel/Pizza.cs
+++ DomainModel/Pizza.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using SharedKernel;
 
 namespace DomainModel
 {
-    public class Class1
+    public class Pizza:ValueObject<Pizza>
     {
     }
 }
